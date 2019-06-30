ef00133cffaa71d8b220b693c9033b952000a6ab
MantasMikal <mikalauskas.it@gmail.com>
2019-06-30 11:13:45 +0100

    Masonry

--- web/src/components/gallery-preview-layout.js
+++ web/src/components/gallery-preview-layout.js
@@ -37,7 +37,7 @@ export default class GalleryPreviewLayout extends React.Component {
       nodes[i] && posts.push(<GalleryPreview item={nodes[i]} key={nodes[i].id} />)
     }
     return (
-      <InfiniteScroll pageStart={0} loadMore={this.loadMore} hasMore={this.state.hasMore} threshold={1850} initialLoad={true}>
+      <InfiniteScroll pageStart={0} loadMore={this.loadMore} hasMore={this.state.hasMore} threshold={1800} initialLoad={true}>
         <MasonryLayout columns={3} gap={25}>
             {posts}
         </MasonryLayout>
