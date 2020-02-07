47ae66ca0a5bec9cd0429590becdc20243344e20
MantasMikal <mikalauskas.it@gmail.com>
2020-02-07 12:51:17 +0000

    fix(site:Cart): Always show item count

--- web/src/components/snipcart/cart.js
+++ web/src/components/snipcart/cart.js
@@ -9,7 +9,7 @@ export default function() {
     <a href="#" className={cn('snipcart-checkout', styles.wrapper)}>
       <FaShoppingCart style={{margin: 'auto', padding: '0.125em 0.425em'}}/>
       <div className={cn("snipcart-summary", styles.itemCount)}>
-        <span className="snipcart-total-items" style={{padding: '0 0.35em', margin: 'auto'}}/>
+        <span className="snipcart-total-items" style={{padding: '0 0.35em', margin: 'auto'}}>0</span>
       </div>
     </a>
   )
