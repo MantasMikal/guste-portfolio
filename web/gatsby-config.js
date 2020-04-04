d26ee2eb754629c794737e3e1a87e4ba7c282504
Mantas <Mikalauskas.it@gmail.com>
2020-04-04 20:17:13 +0100

    Update gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         // and add a token with read permissions
         token: process.env.SANITY_TOKEN,
         watchMode: true,
-        overlayDrafts:true
+        overlayDrafts:false
       }
     },
     {
