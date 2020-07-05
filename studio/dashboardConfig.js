1f0c0bb41ac1f75bd6fe6b3d5b7a430658341633
MantasMikal <mikalauskas.it@gmail.com>
2020-07-05 18:08:21 +0100

    feat: adds ability to deploy from studio

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'Netlify deploys',
+          sites: [
+            {
+              title: 'Website',
+              apiId: 'a6402da5-2b3a-4a22-9b24-1d6ed2908dca',
+              buildHookId: '5e80d0d0d2979dca75272b1c',
+              name: 'gustevasil'
+            }
+          ]
+        }
+      }
+    ]
+  }
