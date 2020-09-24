2f63f2c10fc80799758c9316b0de040e65644549
MantasMikal <mikalauskas.it@gmail.com>
2020-09-24 19:31:22 +0100

    feat: changes image on hover instead of mouse move

--- web/src/components/product/product-preview.js
+++ web/src/components/product/product-preview.js
@@ -1,37 +1,27 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import { cn } from '../../lib/helpers'
-import Image from '../image/multiImage'
+import Image from '../image/image'
 
 import styles from './product-preview.module.css'
 import { responsiveTitle4 } from '../typography.module.css'
 
 function ProductPreview({ images, variants, title, handle, gridLayout }) {
   const { price } = variants[0]
-
-  const mainImages = [
-    {
-      asset: {
-        id: images[0] && images[0].id,
-        fluid: images[0] && images[0].localFile.childImageSharp.fluid,
-        alt: title
-      }
-    },
-    images[1] && {
-      asset: {
-        id: images[1] && images[1].id,
-        fluid: images[1] && images[1].localFile.childImageSharp.fluid,
-        alt: title
-      }
-    }
-  ]
-
   return (
     <div className={cn(styles.itemWrapper, gridLayout && styles.gridLayout)}>
       <Link to={`/store/${handle}`}>
         <div className={styles.inner}>
-          {images.length > 0 && (
-            <Image images={mainImages} square />
+          {images.length > 1 && images[1].localFile.childImageSharp && (
+            <div className={styles.Image}>
+              <Image fluid={images[1].localFile.childImageSharp.fluid} />
+            </div>
+          )}
+
+          {images.length > 0 && images[0] && images[0].localFile.childImageSharp && (
+            <div className={styles.Image}>
+              <Image className={styles.Image} fluid={images[0].localFile.childImageSharp.fluid} />
+            </div>
           )}
         </div>
         <div className={styles.titleWrapper}>
