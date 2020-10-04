1c030f915d67fbc8e425b404fcd3aba00e73d0c1
MantasMikal <mikalauskas.it@gmail.com>
2020-10-04 13:06:00 +0100

    feat: removes console logs

--- web/src/pages/store.js
+++ web/src/pages/store.js
@@ -61,17 +61,13 @@ const StorePage = () => {
   )
   const galleryNodes = products && mapEdgesToNodes(products)
   const categories = collectCategories(galleryNodes)
-  console.log('categories', categories)
   const [queryCat, setQueryCat] = useQueryParam('category', StringParam)
-  console.log('queryCat', queryCat)
 
   const [grid, setGrid] = useState(false)
   const [showFilter, setShowFilter] = useState({ show: true, wasClicked: false })
   const [activeFilter, setActiveFilter] = useState('Prints')
-  console.log('activeFilter', activeFilter)
 
   useEffect(() => {
-    console.log('Setting active filter... to be', queryCat)
     setActiveFilter(queryCat)
   })
 
