b8be9dbc3a12388855974779eb4adb77ec05f321
MantasMikal <mikalauskas.it@gmail.com>
2020-10-04 21:15:08 +0100

    feat: color picker tweaks

--- web/src/components/ColorPallet/index.jsx
+++ web/src/components/ColorPallet/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { array } from 'prop-types'
-
 import styles from './ColorPallet.module.css'
 
-const ColorPallet = ({ colors, onColorClick }) => {
+const ColorPallet = ({ colors, onColorClick, onClear }) => {
   return (
     <div className={styles.ColorPallet}>
       {colors.map((color, i) => (
-        <button onClick={() => onColorClick(color)} style={{ backgroundColor: `#${color}` }} key={`COLOR-${i}`} className={styles.Color} />
+        <button
+          onClick={() => onColorClick(color)}
+          style={{ backgroundColor: `#${color}` }}
+          key={`COLOR-${i}`}
+          className={styles.Color}
+        />
       ))}
     </div>
   )
