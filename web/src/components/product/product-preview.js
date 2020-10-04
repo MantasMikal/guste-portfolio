438a6972c5c4da5d13ea93c678d646381cce11e1
MantasMikal <mikalauskas.it@gmail.com>
2020-10-04 12:38:58 +0100

    fix: product image loading optimisation

--- web/src/components/product/product-preview.js
+++ web/src/components/product/product-preview.js
@@ -1,23 +1,24 @@
 import { Link } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 import { cn } from '../../lib/helpers'
 import Image from '../image/image'
 
 import styles from './product-preview.module.css'
 import { responsiveTitle4 } from '../typography.module.css'
 
-function ProductPreview({ images, variants, title, handle, gridLayout }) {
+const ProductPreview = ({ images, variants, title, handle, gridLayout }) => {
   const { price } = variants[0]
+  const [isLoaded, setLoaded] = useState(false)
+  console.log('ProductPreview -> isVisible', isLoaded)
   return (
     <div className={cn(styles.itemWrapper, gridLayout && styles.gridLayout)}>
-      <Link to={`/store/${handle}`}>
+      <Link to={`/store/${handle}`} onMouseEnter={() => setLoaded(true)}>
         <div className={styles.inner}>
-          {images.length > 1 && images[1].localFile.childImageSharp && (
+          {images.length > 1 && images[1].localFile.childImageSharp && isLoaded && (
             <div className={styles.Image}>
               <Image fluid={images[1].localFile.childImageSharp.fluid} />
             </div>
           )}
-
           {images.length > 0 && images[0] && images[0].localFile.childImageSharp && (
             <div className={styles.Image}>
               <Image className={styles.Image} fluid={images[0].localFile.childImageSharp.fluid} />
