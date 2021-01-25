5a380c517c3dbc18140831505c20f639553bb026
MantasMikal <mikalauskas.it@gmail.com>
2021-01-25 12:15:31 +0000

    feat: adds hotjar

--- web/src/components/head/head.js
+++ web/src/components/head/head.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import { Location } from '@reach/router'
@@ -82,7 +82,7 @@ const Head = ({
       />
       <link href='/icons/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
       <link href='/icons/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
-      <link rel="stylesheet" href="https://use.typekit.net/ehl7qpa.css" />
+      <link rel='stylesheet' href='https://use.typekit.net/ehl7qpa.css' />
       <script type='application/ld+json'>
         {JSON.stringify(
           schemaGenerator({
@@ -98,44 +98,65 @@ const Head = ({
     </Helmet>
   )
 }
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query SiteSettings {
-        allSanitySiteSettings {
-          nodes {
-            title
-            titleShort
-            description
-            siteUrl
-            themeColor
-            backgroundColor
-            logo {
-              asset {
-                url
-                label
-                id
-                size
-                url
+export default props => {
+  useEffect(() => {
+    const s = document.createElement('script')
+    s.type = 'text/javascript'
+    s.async = true
+    s.innerHTML = `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2213358,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+    document.head.appendChild(s)
+    return () => {
+      document.head.removeChild(s)
+    }
+  }, [])
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteSettings {
+          allSanitySiteSettings {
+            nodes {
+              title
+              titleShort
+              description
+              siteUrl
+              themeColor
+              backgroundColor
+              logo {
+                asset {
+                  url
+                  label
+                  id
+                  size
+                  url
+                }
               }
             }
           }
         }
-      }
-    `}
-    render={data => (
-      <Location>
-        {({ location }) => {
-          return (
-            <Head
-              {...data.allSanitySiteSettings.nodes}
-              logo={data.allSanitySiteSettings.nodes[0].logo.asset.url}
-              {...props}
-              location={location}
-            />
-          )
-        }}
-      </Location>
-    )}
-  />
-)
+      `}
+      render={data => (
+        <Location>
+          {({ location }) => {
+            return (
+              <Head
+                {...data.allSanitySiteSettings.nodes}
+                logo={data.allSanitySiteSettings.nodes[0].logo.asset.url}
+                {...props}
+                location={location}
+              />
+            )
+          }}
+        </Location>
+      )}
+    />
+  )
+}
