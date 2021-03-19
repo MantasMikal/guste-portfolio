0792a4409ca0f20f16753cafbb55ef36d75d0c33
MantasMikal <mikalauskas.it@gmail.com>
2021-03-19 14:23:59 +0000

    feat: updates gallery schema

--- studio/schemas/gallery.js
+++ studio/schemas/gallery.js
@@ -1,65 +1,65 @@
 export default {
-  name: 'gallery',
-  title: 'Gallery Items',
-  type: 'document',
+  name: "gallery",
+  title: "Gallery Items",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
       options: {
         isHighlighted: true
       }
     },
     {
-      name: 'mainImage',
-      title: 'Main image',
-      description: 'Main image of the document (thumbnail)',
-      type: 'mainImage'
+      name: "mainImage",
+      title: "Main image",
+      description: "Main image of the document (thumbnail)",
+      type: "mainImage"
     },
     {
-      name: 'mainVideo',
+      name: "mainVideo",
       title: "Main video",
-      type: 'file'
+      type: "file"
     },
     {
-      name: 'publishedAt',
-      title: 'Published at',
-      description: 'You can use this field to schedule projects where you show them',
-      type: 'datetime'
+      name: "publishedAt",
+      title: "Published at",
+      description: "You can use this field to schedule projects where you show them",
+      type: "datetime"
     },
     {
-      name: 'artworkCategory',
-      title: 'Artwork Category',
-      type: 'array',
-      of: [{ type: 'reference', to: { type: 'artworkCategory' } }]
-    },  
-    {
-      name: 'excerpt',
-      title: 'Excerpt',
-      description: 'Short post description',
-      type: 'blockText'
+      name: "artworkCategory",
+      title: "Artwork Category",
+      type: "array",
+      of: [{ type: "reference", to: { type: "artworkCategory" } }]
     },
     {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent'
+      name: "excerpt",
+      title: "Excerpt",
+      description: "Short post description",
+      type: "blockText"
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent"
     }
   ],
   preview: {
     select: {
-      title: 'title',
-      image: 'mainImage',
-      publishedAt: 'publishedAt'
+      title: "title",
+      image: "mainImage",
+      publishedAt: "publishedAt"
     },
-    prepare ({ title = 'No title', publishedAt, image }) {
+    prepare({ title = "No title", publishedAt, image }) {
       return {
         title,
         media: image,
         subtitle: publishedAt
           ? new Date(publishedAt).toLocaleDateString()
-          : 'Missing publishing date'
-      }
+          : "Missing publishing date"
+      };
     }
   }
-}
+};
