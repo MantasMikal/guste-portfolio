fbb3a32200a507056bf437a9207cda3b83aafb14
Mantas Mikalauskas <mantas.mikalauskas@clock.co.uk>
2021-04-14 13:55:55 +0100

    fix: removes old meta tags

--- web/src/components/head/head.js
+++ web/src/components/head/head.js
@@ -35,13 +35,8 @@ const Head = ({
       <meta content='black-translucent' name='apple-mobile-web-app-status-bar-style' />
       <meta content={themeColor} name='theme-color' />
       <meta content={title} name='application-name' />
-
       <meta content='website' property='og:type' />
       <meta content={title} property='og:site_name' />
-      {/* <meta content={social.fbAppId} property="fb:app_id" />
-    <meta content="summary_large_image" name="twitter:card" />
-    <meta content={`@${social.twitter}`} name="twitter:site" />
-    <meta content={`@${social.twitter}`} name="twitter:creator" /> */}
       <meta content={pageTitleFull} name='twitter:text:title' />
       <meta content={canonical} property='og:url' />
       <meta content={canonical} name='twitter:url' />
@@ -56,32 +51,8 @@ const Head = ({
       <meta content='1024' property='og:image:width' />
       <meta content='512' property='og:image:height' />
 
-      <meta content={themeColor} name='msapplication-TileColor' />
-      <meta content='/icons/mstile-70x70.png' name='msapplication-square70x70' />
-      <meta content='/icons/mstile-144x144.png' name='msapplication-square144x144' />
-      <meta content='/icons/mstile-150x150.png' name='msapplication-square150x150' />
-      <meta content='/icons/mstile-310x150.png' name='msapplication-wide310x150' />
-      <meta content='/icons/mstile-310x310.png' name='msapplication-square310x310' />
-
       <link href='/manifest.json' rel='manifest' />
 
-      <link href='/icons/apple-touch-icon-57x57.png' rel='apple-touch-icon' sizes='57x57' />
-      <link href='/icons/apple-touch-icon-60x60.png' rel='apple-touch-icon' sizes='60x60' />
-      <link href='/icons/apple-touch-icon-72x72.png' rel='apple-touch-icon' sizes='72x72' />
-      <link href='/icons/apple-touch-icon-76x76.png' rel='apple-touch-icon' sizes='76x76' />
-      <link href='/icons/apple-touch-icon-114x114.png' rel='apple-touch-icon' sizes='114x114' />
-      <link href='/icons/apple-touch-icon-120x120.png' rel='apple-touch-icon' sizes='120x120' />
-      <link href='/icons/apple-touch-icon-144x144.png' rel='apple-touch-icon' sizes='144x144' />
-      <link href='/icons/apple-touch-icon-152x152.png' rel='apple-touch-icon' sizes='152x152' />
-      <link href='/icons/apple-touch-icon-167x167.png' rel='apple-touch-icon' sizes='167x167' />
-      <link
-        href='/icons/apple-touch-icon-180x180.png'
-        rel='icon'
-        sizes='180x180'
-        type='image/png'
-      />
-      <link href='/icons/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
-      <link href='/icons/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
       <link rel='stylesheet' href='https://use.typekit.net/ehl7qpa.css' />
       <script type='application/ld+json'>
         {JSON.stringify(
