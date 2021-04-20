0095f62b90553055ef8f5a520c2b03cd88f31ebe
Mantas Mikalauskas <mantas.mikalauskas@clock.co.uk>
2021-04-20 19:28:48 +0100

    fix: updates icons

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { FaBehanceSquare, FaEnvelope, FaInstagramSquare, FaLinkedin } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { GrInstagram } from 'react-icons/gr'
+import { HiOutlineMail } from 'react-icons/hi'
+import { SiBehance } from 'react-icons/si'
+
 import styles from './contact.module.css'
 import SEO from '../components/seo'
 
@@ -7,42 +11,42 @@ const Contacts = props => {
   const { title, _rawBody } = props.contactInfo
   return (
     <div className={styles.wrapper}>
-      <SEO title='About' />
+      <SEO title="About" />
 
       <div className={styles.grid}>
         <h1 className={styles.title}>Hi!</h1>
         <div className={styles.iconGrid}>
           <a
             className={styles.icon}
-            href='mailto:hi@guste.design'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="mailto:hi@guste.design"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <FaEnvelope size='2em' />
+            <HiOutlineMail size="2em" />
           </a>
           <a
             className={styles.icon}
-            href='https://www.instagram.com/guste.design'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.instagram.com/guste.design"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <FaInstagramSquare size='2em' />
+            <GrInstagram size="2em" />
           </a>
           <a
             className={styles.icon}
-            href='https://www.behance.net/GusteDesign'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.behance.net/GusteDesign"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <FaBehanceSquare size='2em' />
+            <SiBehance size="2em" />
           </a>
           <a
             className={styles.icon}
-            href='https://www.linkedin.com/in/guste-vasil/'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.linkedin.com/in/guste-vasil/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <FaLinkedin size='2em' />
+            <FaLinkedin size="2em" />
           </a>
         </div>
       </div>
